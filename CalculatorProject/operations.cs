6916a4cd2695885756c8d93c2693602a61d30352
tavisca-vkamat <vkamat@tavisca.com>
2016-08-22 18:06:27 +0530

    Revert "Merging vkamat"

--- CalculatorProject/operations.cs
+++ CalculatorProject/operations.cs
@@ -9,26 +9,10 @@ namespace CalculatorProject
     class Operations
     {
 
-        bool validateNumbers(string number1, string number2)
+        bool validateNumbers(double number1, double number2)
         {
-            double r=0.0;
-            bool one = double.TryParse(number1 , out r);
-            bool two = double.TryParse(number2 , out r);
-
-            return one && two;
+            return false;
         }
         /* addition subtraction multiplication division*/
-
-        public string multiplication(string First_number, string Second_number)
-        {
-            if (validateNumbers(First_number, Second_number))
-            {
-                string result = Convert.ToString(double.Parse(First_number) * double.Parse(Second_number));
-                return result;
-            }
-            else
-                return "Invalid number ! ";
-
-        }
     }
 }
